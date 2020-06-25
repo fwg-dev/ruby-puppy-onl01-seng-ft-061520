0d6eafd88ddb38035b38560114c77cd3b368b062
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-25 03:57:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,10 +18,10 @@ attr_accessor :name
   end
 # # private
 def self.print_all
-  @@all .each do |x| 
-    puts x.name 
+  @@all .each do |x|
+    puts x.name
     # my_array == @@all.map do |
-  end 
+  end
 end
 end
 
