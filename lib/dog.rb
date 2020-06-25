def4dc4868e523fd725da13b37a097f10d494d74
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-25 03:37:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,17 +6,17 @@ attr_accessor :name
   def initialize(name)
     @name = name
     @@all << self
-    @@clear_all = clear_all
+  
   end
 
   def self.all
     @@all
-
   end
 
   def self.clear_all
     @@all.clear
   end
+  
 end
 
 
